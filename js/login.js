1e4c5504314944dda9beaf0db02b178be564d753
Isai Perez <72577853+alfredo2201@users.noreply.github.com>
2022-04-22 13:31:13 -0700

    Funciona el iniciar sesion y el registrarse

--- js/login.js
+++ js/login.js
@@ -1,85 +1,98 @@
 
-function register(){
+function registerUser() {
     var name = document.getElementById("txt_name").value;
     var email = document.getElementById("txt_email").value;
-    var password = document.getElementById("txt_password").value;    
+    var password = document.getElementById("txt_password").value;
     var repeat_password = document.getElementById("txt_repeat_password").value;
-    
-    if(name == null || name == ""){
+
+    if (name == null || name == "") {
         alert("Por favor ingrese valor al campo de texto")
         return
     }
-    if(email == null || email == ""){
+    if (email == null || email == "") {
         alert("Por favor ingrese valor al campo de texto")
         return
     }
-    if(password == null || password == ""){
+    if (password == null || password == "") {
         alert("Por favor ingrese valor al campo de texto")
         return
     }
-    if(repeat_password == null || repeat_password == ""){
+    if (repeat_password == null || repeat_password == "") {
         alert("Por favor ingrese valor al campo de texto")
         return
     }
-    if(password !== repeat_password){
+    if (password !== repeat_password) {
         alert("Las contraseñas no coinciden.\nVuelva a intentarlo")
         return
     }
 
     $.ajax({
-        url:"http://localhost:2424/users/saveUser",
-        type:"POST",        
-        headers: {           
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*"},
-        data:JSON.stringify({
-            "name":name,
-            "email":email,
-            "password":password
-        }),processData : false,        
-        success : function(data) {            
-           if (data.status == "ok") {
-               alert(data.message)               
-               window.location.href = "index.html"
-           }
-           else {
-                alert ("El nombre de usuario o la contraseña son incorrectos, verifique con cuidado ~~") // Error al iniciar sesión
-           }
-       },
-       error:function (errorThrown) {            
-            alert("¡El nombre de usuario o la contraseña son incorrectos!")            
-       }
+        url: "http://localhost:2424/users/saveUser",
+        type: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        data: JSON.stringify({
+            "id": null,
+            "name": name,
+            "email": email,
+            "password": password
+        }), processData: false,
+        success: function (resp) {                    
+            if (resp == "ok") {
+                alert("Usuario creado exitosamente...")
+                window.location.href = "home.html"
+            }
+            else {
+                alert("El nombre de usuario o la contraseña son incorrectos, verifique con cuidado ~~") // Error al iniciar sesión
+            }
+        },
+        error: function (errorThrown, estado) {
+            alert("Ah ocurrido un error: " + estado)
+        }
     })
 
 }
 
-function loginUser(){
-    let email = document.getElementById("txt_email")
-    let password = document.getElementById("txt_password")
+function loginUser() {
+    let email = document.getElementById("txt_email").value
+    let password = document.getElementById("txt_password").value
+
+    if(email == "" && password == ""){
+        alert("Campo de correo y constraseña están vacio\nIngrese un valor.")
+        return
+    }
+    if(email == "" || email == null) {
+        alert("Campo de correo está vacio\nIngrese un valor.")
+        return
+    }
+    if(password == "" || password == null) {
+        alert("Campo de contraseña está vacio\nIngrese un valor.")
+        return
+    }
     $.ajax({
-        url:"http://localhost:2424/users/getUser",
-        type:"GET",        
-        headers: {           
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*"},
-        data:JSON.stringify({            
-            "email":email,
-            "password":password
-        }),processData : false,        
-        success : function(data) {            
-           if (data.status == "ok") {
-               alert(data.message)               
-               window.location.href = "index.html"
-           }
-           else {
-                alert ("El email o la contraseña son incorrectos, verifique con cuidado ~~") // Error al iniciar sesión
-           }
-       },
-       error:function (errorThrown) {            
-            alert("¡El email o la contraseña son incorrectos!")            
-       }
+        url: "http://localhost:2424/users/getUser/?email="+email+"&password="+password,
+        type: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        dataType: 'json',
+        processData: false,
+        success: function (resp) {
+            if (resp !== null) {                
+                window.location.href = "home.html"
+            }
+            else {
+                alert("El email o la contraseña son incorrectos, verifique con cuidado ~~") // Error al iniciar sesión
+            }
+        },
+        error: function (errorThrown) {
+            alert("Ah ocurrido un error: " + errorThrown)
+        }
     })
 
 }
