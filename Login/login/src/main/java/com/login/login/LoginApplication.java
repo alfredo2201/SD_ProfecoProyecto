a1125bcc9926dde8acd5a05498e494b976abe1dc
Isai Perez <72577853+alfredo2201@users.noreply.github.com>
2022-05-01 20:10:28 -0700

    Configuracion de las apis

--- Login/login/src/main/java/com/login/login/LoginApplication.java
+++ Login/login/src/main/java/com/login/login/LoginApplication.java
@@ -2,6 +2,8 @@ package com.login.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,8 +17,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 	"com.login.repository"
 }
 )
-public class LoginApplication {
+public class LoginApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LoginApplication.class);
+    }
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 	}
